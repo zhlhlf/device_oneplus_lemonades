73d8c71cb826140f146609e79ec0a78037dee663
Yuan Si <do4suki@gmail.com>
2022-08-19 20:39:15 +0800

    lemonades: Convert to AOSPA tree
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: I909cf0ac0006c5047de9b24205b433def4f0fc2f

--- aospa_lemonades.mk
+++ aospa_lemonades.mk
@@ -6,22 +6,22 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from lemonades device
 $(call inherit-product, device/oneplus/lemonades/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
-PRODUCT_NAME := lineage_lemonades
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aospa_lemonades
 PRODUCT_DEVICE := lemonades
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2101
-
-PRODUCT_SYSTEM_NAME := OnePlus9R
-PRODUCT_SYSTEM_DEVICE := OnePlus9R
+PRODUCT_MODEL := OnePlus9R
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
